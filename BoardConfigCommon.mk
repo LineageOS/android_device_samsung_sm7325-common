548f0989dfaa0b4478c508e610118e2b980d83cc
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-12 23:36:23 -0400

    sm7325-common: Kill deprecated/unneeded board flags
    * TARGET_KERNEL_ARCH - no-op unless it differs from TARGET_ARCH.
    * TARGET_KERNEL_HEADER_ARCH - no-op unless it differs from TARGET_ARCH.
    * TARGET_LINUX_KERNEL_VERSION - Vendor specific.
    
    Change-Id: I8d140fecdbbaac979bc8398745ffa95b911e4c89

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,9 +52,6 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
 
 # Kernel config
 TARGET_KERNEL_SOURCE        := kernel/samsung/sm7325
-TARGET_KERNEL_ARCH          := arm64
-TARGET_KERNEL_HEADER_ARCH   := arm64
-TARGET_LINUX_KERNEL_VERSION := 5.4
 
 # Kernel flags
 BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image pcie_ports=compat loop.max_part=7 iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 printk.devkmsg=on
