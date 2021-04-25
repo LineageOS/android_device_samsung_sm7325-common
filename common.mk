adfde7d9f0d37e4c3102a92fc737cefcc2bbe1c9
Tim Zimmerman <tim@linux4.de>
2021-04-25 15:24:58 +0200

    sm7325-common: nfc: move data to /data/vendor/nfc
    Change-Id: Ia8d3d8bc7f1a8ad1953215538c9c6094092c7b83

--- common.mk
+++ common.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf \
-    $(COMMON_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf
+    $(COMMON_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # OMX
 PRODUCT_PACKAGES += \
