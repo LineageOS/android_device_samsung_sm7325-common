7298356dc4efc5706fe4e07e3ebc4f0f295ea7ad
Simon1511 <me@simon1511.de>
2024-03-17 12:43:36 +0100

    sm7325-common: switch to U RIL
    - Pull RIL and QMI blobs from A528BXXS5FWL4 (U)
    - Build sehradiomanager for working signal strength
    - Update SMSC and slotID patches accordingly
    - Build vendor.samsung.hardware.radio@2.[0,1,2] from source
    
    Change-Id: I5ee7e727133d421cc0d37eeacf647669cac2561b

--- common.mk
+++ common.mk
@@ -368,7 +368,8 @@ PRODUCT_PACKAGES += \
     libxml2 \
     librilutils \
     librmnetctl \
-    secril_config_svc
+    secril_config_svc \
+    sehradiomanager
 
 # Sensors
 PRODUCT_PACKAGES += \
