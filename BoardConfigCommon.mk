69e59bd6ecd6ef8ced565e6876571a4771613f40
Andrzej Szafran <rtd1250@gmail.com>
2024-07-27 13:19:57 +0200

    sm7325-common: Include hardware/samsung FCM
    Change-Id: Icb1a1bd6de56cbf2419cb8a046fa2a7a35cfc912

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,6 +180,7 @@ DEVICE_MANIFEST_YUPIK_FILES += $(COMMON_PATH)/configs/manifest_yupik.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # QCOM
