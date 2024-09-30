d475e46ce0a2eb477f3173d60496fb3971519655
Andreas Schneider <asn@cryptomilk.org>
2024-09-30 23:04:48 +0200

    sm7325-common: Replace prebuilt libsensorndkbridge with a shim
    Change-Id: Id94ed43336e84934f42f56c58e506b50af3c59fd

--- extract-files.py
+++ extract-files.py
@@ -87,6 +87,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         .call(blob_fixup_ril_smsc),
+    ('vendor/lib64/libhypermotion_core.so', 'vendor/lib64/libsensorlistener.so', 'vendor/lib64/libvdis_core.so'): blob_fixup()
+        .add_needed('libshim_sensorndkbridge.so'),
     ('vendor/lib64/hw/gatekeeper.mdfpp.so', 'vendor/lib64/libkeymaster_helper.so', 'vendor/lib64/libskeymaster4device.so'): blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     ('vendor/lib/libwvhidl.so', 'vendor/lib/mediadrm/libwvdrmengine.so'): blob_fixup()
