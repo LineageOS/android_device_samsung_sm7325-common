ccc266939c4eefc2e9aec73d5c0099df4762ace2
Bruno Martins <bgcngm@gmail.com>
2024-12-30 18:58:19 +0000

    sm7325-common: Remove no longer needed local FCM
    Change-Id: If329270e4874dd77f4ae6fe2d8b7baff5437de0d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,7 +158,6 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
