8b4c40227cb31ee442d52501cee561e6fe5e7e45
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    sm7325-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- common.mk
+++ common.mk
@@ -146,7 +146,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
-    gralloc.default \
     vendor.qti.hardware.memtrack-service \
     AdvancedDisplay
 
