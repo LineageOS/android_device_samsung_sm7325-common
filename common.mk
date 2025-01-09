f488deb24af13993a0fdc1baac04bd738891c869
LuK1337 <priv.luk@gmail.com>
2025-01-09 12:57:53 +0100

    sm7325-common: Build gralloc.qcom
    Camera HAL wants it and crashes if it loads default gralloc
    implementation instead.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8059
    Change-Id: I47965713c822cfdfc369975f8975f21f70133608

--- common.mk
+++ common.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    gralloc.qcom \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
     vendor.qti.hardware.memtrack-service \
