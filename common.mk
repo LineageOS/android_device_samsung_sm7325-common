98dbe56ada760f47a15ae14a88dd5629451dd9fd
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 14:48:54 +0800

    sm7325-common: Build QTI Thermal AIDL HAL
    Change-Id: I21406af9d90fe6be1402f30e1b19958e4f46eec9

--- common.mk
+++ common.mk
@@ -380,7 +380,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor \
+    android.hardware.thermal-service.qti \
     android.frameworks.cameraservice.service@2.1.vendor \
     libcamera_metadata.vendor
 
