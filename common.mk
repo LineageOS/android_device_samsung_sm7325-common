4c61e59ae23f066017922c8dd0a1248bb1b76553
Atakan <ata.kaner03@gmail.com>
2025-08-30 06:24:21 +0300

    sm7325-common: Build soundtrigger@2.3-impl
    Match with stock vendor and manifest entry.
    
    Change-Id: I520ea56bade1814562d6a3496200ec4dd48fe376

--- common.mk
+++ common.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl.samsung-sm7325 \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.r_submix.default \
     audio.usb.default \
     libtinycompress \
