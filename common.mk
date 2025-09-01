44b34eea9585c56e7453dd6e78cda9d61ed043e4
Bruno Martins <bgcngm@gmail.com>
2025-09-01 01:43:37 +0100

    sm7325-common: Migrate to LiveDisplay AIDL HAL
    Change-Id: Iafa206bb882cc49866a4b94b3ac03f15c4399c30

--- common.mk
+++ common.mk
@@ -238,7 +238,7 @@ $(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/s
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-qcom
+    vendor.lineage.livedisplay-service.samsung-qcom
 
 # Media
 PRODUCT_PACKAGES += \
