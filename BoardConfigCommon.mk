99c4e96688e3c76e014aeb8cae5b17527687b9b8
anhdat1024 <tadat1024@gmail.com>
2025-10-01 09:00:58 +0000

    sm7325-common: Switch to DEVICE_MANIFEST_FILE
    Change-Id: Id079d51e4ceafb511dea5aeca74e878e18696027

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,8 +155,7 @@ AUDIO_FEATURE_ENABLED_SVA_MULTI_STAGE := true
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 
 # HIDL manifests
-DEVICE_MANIFEST_SKUS := yupik
-DEVICE_MANIFEST_YUPIK_FILES += $(COMMON_PATH)/configs/manifest_yupik.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/framework_compatibility_matrix.xml \
