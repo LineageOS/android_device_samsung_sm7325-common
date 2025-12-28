db51e9ccc40d920c0fc617a7510e0412bf90ab21
Bush-cat <bushccat@gmail.com>
2025-12-28 16:38:16 +0000

    sm7325-common: Pull everything from A736BXXSAGYJ3
    Change-Id: I34ce3e093e7c25740011ca6df46a6d16345468f4

--- common.mk
+++ common.mk
@@ -57,6 +57,7 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.vendor.onebinary.rc \
     vendor.samsung.rilchip.qcom.rc \
+    vendor.samsung.rild.rc \
     init.vendor.rilcommon.rc \
     init.vendor.sensors.rc \
     ueventd.qcom.rc \
